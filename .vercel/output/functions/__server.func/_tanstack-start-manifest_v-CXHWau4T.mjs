f597eed0ab23a6d7594990e124f7e9be718abea9
wbarcellosn <wbarcellosn@gmail.com>
2026-06-15 22:49:32 -0300

    Sync latest local project

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CXHWau4T.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Dev/spin-win-hub-main/spin-win-hub-main/src/routes/__root.tsx", children: ["/", "/admin"], preloads: ["/assets/index-CXy0d43l.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CXy0d43l.js" } }] }, "/": { filePath: "C:/Dev/spin-win-hub-main/spin-win-hub-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Bi3-lKM2.js", "/assets/logo-CzVGUUcE.js"] }, "/admin": { filePath: "C:/Dev/spin-win-hub-main/spin-win-hub-main/src/routes/admin.tsx", children: void 0, preloads: ["/assets/admin-DFIfbBKu.js", "/assets/logo-CzVGUUcE.js"] } } });
-export {
-  tsrStartManifest
-};
